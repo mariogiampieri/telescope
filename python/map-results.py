e443b5dc77c260f09f6a8c77913b8ce07f0ea735
mariogiampieri <mariogiampieri@gmail.com>
2018-09-02 17:11:26 -0400

    add tutorial reference

--- python/map-results.py
+++ python/map-results.py
@@ -1,3 +1,5 @@
 import pandas as pd
 import geopandas as gpd
 import folium
+
+# refer to https://nbviewer.jupyter.org/github/vincentropy/python_cartography_tutorial/blob/master/part1_basic_folium_maps.ipynb for a tutorial on mapping in jupyter notebooks
